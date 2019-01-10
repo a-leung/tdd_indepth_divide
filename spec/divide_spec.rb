f1ccf3205e2688667ac14f260fa12ca37a473398
Andrew Leung <andrew@ualberta.net>
2019-01-10 23:29:52 +0000

    Test: Add Arguments

--- spec/divide_spec.rb
+++ spec/divide_spec.rb
@@ -9,3 +9,9 @@ describe 'divide' do
     expect { divide }.to_not raise_error
   end
 end
+
+describe 'arguments' do
+  it 'accepts two arguments - numerator & denominator' do
+    expect { divide(1,1) }.to_not raise_error
+  end
+end
